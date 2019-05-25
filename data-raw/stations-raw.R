9ed66e3bf27c9e0b722b3ea46f73db46a8be2beb
fawda123 <mbafs2012@gmail.com>
2019-05-24 23:00:38 -0700

    added stations data file

---
+++ data-raw/stations-raw.R
@@ -0,0 +1,16 @@
+# EPC station, segment data file ---------------------------------------------
+
+library(tidyverse)
+
+# stations by bay segment
+stations <- list(
+  OTB = c(36, 38, 40, 41, 46, 47, 50, 51, 60, 63, 64, 65, 66, 67, 68),
+  HB = c(6, 7, 8, 44, 52, 55, 70, 71, 73, 80),
+  MTB = c(9, 11, 81, 84, 13, 14, 32, 33, 16, 19, 28, 82),
+  LTB = c(23, 24, 25, 90, 91, 92, 93, 95)
+  ) %>%
+  enframe('bay_segment', 'station') %>%
+  unnest %>%
+  data.frame(stringsAsFactors = F)
+
+save(stations, file = 'data/stations.RData', compress = 'xz')
