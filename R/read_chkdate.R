854ca79b909b8887abaf0238690a9c149dd21a49
fawda123 <mbafs2012@gmail.com>
2019-06-29 14:54:16 -0700

    added read_chkdate function, concepts to site yaml

---
+++ R/read_chkdate.R
@@ -0,0 +1,42 @@
+#' Compare date of local xlsx file with the same file on the server
+#'
+#' @param epchc_url chr string of full path to file on the server
+#' @param xlsx chr string of full path to local file
+#'
+#' @return A logical vector indicating if the local file is current
+#'
+#' @importFrom magrittr %>%
+#'
+#' @family read
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' epchc_url <- "ftp://ftp.epchc.org/EPC_ERM_FTP/WQM_Reports/RWMDataSpreadsheet_ThroughCurrentReportMonth.xlsx"
+#' xlsx <- file.path(dir_gdrive, "data/wq/2018_Results_Updated.xls")
+#' read_chkdate(epchc_url, xlsx)
+#' }
+read_chkdate <- function(epchc_url, xlsx) {
+
+  # get date of file on server
+  con <- epchc_url %>%
+    dirname %>%
+    paste0(., '/') %>%
+    curl::curl()
+  dat <- readLines(con)
+  close(con)
+  srdate <- dat %>%
+    grep(basename(epchc_url), ., value = TRUE) %>%
+    gsub('^(.*AM|.*PM).*$', '\\1', .) %>%
+    lubridate::mdy_hm(.)
+
+  # get date of local file
+  lcdate <- file.info(xlsx)$mtime
+
+  # check if serer date is less than or equal to local file date
+  is_latest <- srdate <= lcdate
+
+  return(is_latest)
+
+}
