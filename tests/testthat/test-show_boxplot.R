ba51ddaab59714c0aea5e41f7477dbe9df7c181e
fawda123 <mbafs2012@gmail.com>
2019-12-02 16:57:14 -0500

    removing some tests

--- tests/testthat/test-show_boxplot.R
+++ tests/testthat/test-show_boxplot.R
@@ -2,6 +2,3 @@ test_that("Checking show_boxplot class", {
   result <- show_boxplot(epcdata)
   expect_is(result, 'ggplot')
 })
-test_that("Checking show_boxplot bay_segment",{
-  expect_error(show_boxplot(epcdata, bay_segment = "lll"), "'arg' should be one of “OTB”, “HB”, “MTB”, “LTB”")
-})
--- tests/testthat/test-show_thrplot.R
+++ tests/testthat/test-show_thrplot.R
@@ -2,6 +2,3 @@ test_that("Checking show_thrplot class", {
   result <- show_thrplot(epcdata)
   expect_is(result, 'ggplot')
 })
-test_that("Checking show_thrplot bay_segment",{
-  expect_error(show_thrplot(epcdata, bay_segment = "lll"), "'arg' should be one of “OTB”, “HB”, “MTB”, “LTB”")
-})
