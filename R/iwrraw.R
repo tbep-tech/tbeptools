fe088f54e43ff30ad856805bb1a8cf75c070dada
fawda123 <mbafs2012@gmail.com>
2019-12-07 22:29:35 -0500

    added iwr run 56 raw data

---
+++ R/iwrraw.R
@@ -0,0 +1,7 @@
+#' FDEP IWR run 56
+#'
+#' Florida Department of Environmental Protection, Impaired Waters Rule, Run 56
+#'
+#' @format A data frame 1224558 rows and 52 variables
+#' @family utilities
+"iwrraw"
