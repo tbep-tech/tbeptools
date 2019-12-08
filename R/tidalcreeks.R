41a9df0ecc5090d896981470cf9a81a4702d8c2f
fawda123 <mbafs2012@gmail.com>
2019-12-07 21:29:41 -0500

    added creek length to tidalcreeks

--- R/tidalcreeks.R
+++ R/tidalcreeks.R
@@ -2,11 +2,12 @@
 #'
 #' Spatial data object of tidal creeks
 #'
-#' @format A simple features \code{\link[sf]{sf}} object (MULTILINESTRING) with 581 feaures and 3 fields, +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
+#' @format A simple features \code{\link[sf]{sf}} object (MULTILINESTRING) with 581 feaures and 4 fields, +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
 #' \describe{
 #'   \item{wbid}{chr}
 #'   \item{JEI}{chr}
 #'   \item{class}{chr}
+#'   \item{Creek_Length_m}{num}
 #' }
 #' @family utilities
 "tidalcreeks"
