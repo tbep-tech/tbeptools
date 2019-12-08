e4625a8601683ef2f15025e4717c0b9138f7fb8f
fawda123 <mbafs2012@gmail.com>
2019-12-07 21:46:44 -0500

    added unique id to tidalcreeks

--- R/tidalcreeks.R
+++ R/tidalcreeks.R
@@ -2,8 +2,9 @@
 #'
 #' Spatial data object of tidal creeks
 #'
-#' @format A simple features \code{\link[sf]{sf}} object (MULTILINESTRING) with 581 feaures and 4 fields, +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
+#' @format A simple features \code{\link[sf]{sf}} object (MULTILINESTRING) with 581 feaures and 5 fields, +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
 #' \describe{
+#'   \item{id}{num}
 #'   \item{wbid}{chr}
 #'   \item{JEI}{chr}
 #'   \item{class}{chr}
