703af60d91f6c8869d9ce73708b3cceb7549cf9e
fawda123 <mbafs2012@gmail.com>
2019-12-09 16:19:18 -0500

    trying to fix output of anlz_tdlcrk for MW results

--- R/anlz_tdlcrk.R
+++ R/anlz_tdlcrk.R
@@ -15,8 +15,6 @@
 #' anlz_tdlcrk(tidalcreeks, iwrraw, yr)
 anlz_tdlcrk <- function(tidalcreeks, iwrraw, yr = 2018) {
 
-  yr <- 2018
-
   # mcodes <- c("CHLAC","CHLA_ ", "COLOR", "COND", "DO", "DOSAT", "DO_MG", "NO23_", "NO3O2", "ORGN", "SALIN", "TKN", "TKN_M", "TN", "TN_MG", "TP", "TPO4_",
   #   "TP_MG", "TSS", "TSS_M", "TURB")
   mcodes <- c("TN", "TN_MG")
@@ -56,17 +54,19 @@ anlz_tdlcrk <- function(tidalcreeks, iwrraw, yr = 2018) {
     dplyr::left_join(iwrdat, by = c('wbid', 'class', 'JEI')) %>%
     dplyr::mutate(
       tn_threshold = dplyr::case_when(
-        grepl('^PC|^LC', JEI) ~ 1.54,
-        T ~ 1.65
+        !is.na(Creek_Length_m) ~ 1.65,
+        !is.na(Creek_Length_m) & grepl('^PC|^LC', JEI) ~ 1.54
       ),
       action = dplyr::case_when(
-        grepl('^PC|^LC', JEI) ~ 1.36,
-        T ~ 1.46
+        !is.na(Creek_Length_m) ~ 1.46,
+        !is.na(Creek_Length_m) & grepl('^PC|^LC', JEI) ~ 1.36
       ),
       caution = dplyr::case_when(
         class %in% c('3M', '2') & tn_threshold == 1.65 ~ 1.46 - 0.0174*(23.78 - (Creek_Length_m/1000)),
         class %in% c('3M', '2') & tn_threshold == 1.54 ~ 1.36 - 0.0174*(23.78 - (Creek_Length_m/1000)),
-      ),
+      )
+    )
+      ,
       grade = dplyr::case_when(
         class %in% c('3F', '1') & TN > tn_threshold ~ 4,
         class %in% c('3F', '1') & TN <= tn_threshold & action <= TN ~ 3,
