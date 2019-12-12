a235400bf199194cb8c3d6e7a306ab3b723f4817
fawda123 <mbafs2012@gmail.com>
2019-12-12 14:22:52 -0500

    added anlz_refs

--- R/anlz_refs.R
+++ R/anlz_refs.R
@@ -1,28 +1,81 @@
-library(tidyverse)
+#' Convert references csv to bib
+#'
+#' Convert references csv to bib
+#'
+#' @param path chr string of path to reference csv file
+#'
+#' @return A data frame with references formatted as bib entries
+#'
+#' @export
+#'
+#' @family analyze
+#'
+#' @examples
+#'
+#' # input and format
+#' path <- 'https://raw.githubusercontent.com/tbep-tech/tbep-refs/master/tbep-refs.csv'
+#' bibs <- anlz_refs(path)
+#'
+#' \dontrun{
+#' # save output
+#'  writeLines(bibs, 'formatted.bib')
+#' }
+anlz_refs <- function(path) {
 
-refs <- read.csv('https://raw.githubusercontent.com/tbep-tech/tbep-refs/master/tbep-refs.csv', stringsAsFactors = F)
+  # import
+  refs <- read.csv(path, stringsAsFactors = F)
 
-flds <- list(
-  article = c('author', 'year', 'title', 'journal', 'volume', 'number', 'pages', 'doi', 'misc', 'url'),
-  techreport = c('author', 'year', 'title', 'number', 'publisher', 'address', 'pages', 'misc', 'url'),
-  incollection = c('author', 'year', 'title', 'number', 'booktitle', 'editor', 'publisher', 'address', 'pages', 'misc', 'url'),
-  proceedings = c('author', 'year', 'title', 'number', 'publisher', 'address', 'misc', 'url')
-)
-
-articles <- refs %>%
-  dplyr::group_by(type) %>%
-  tidyr::nest() %>%
-  dplyr::mutate(
-    data = purrr::pmap(list(type, data), function(type, data){
-      browser()
-      fldsel <- flds[[type]]
-
-      out <- data %>%
-        select_at(c('index', fldsel)) %>%
-        mutate_at(fldsel, ~paste0('={', ., '},')) %>%
-        gather('var', 'val', -index) %>%
-        unite('val', var, val, sep = '')
-
-    })
+  # specific fields for bib entry types
+  flds <- list(
+    article = c('author', 'year', 'title', 'journal', 'volume', 'number', 'pages', 'doi', 'misc', 'url'),
+    techreport = c('author', 'year', 'title', 'number', 'publisher', 'address', 'pages', 'misc', 'url'),
+    incollection = c('author', 'year', 'title', 'number', 'booktitle', 'editor', 'publisher', 'address', 'pages', 'misc', 'url'),
+    proceedings = c('author', 'year', 'title', 'number', 'publisher', 'address', 'misc', 'url')
   )
+
+  # format refernce csv as bib
+  out <- refs %>%
+    dplyr::group_by(type) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(
+      data = purrr::pmap(list(type, data), function(type, data){
+
+        fldsel <- flds[[type]]
+
+        levs <- c('tag', fldsel, 'brk')
+
+        tmp <- data %>%
+          dplyr::mutate(
+            tag = paste0('@', type, '{', tag, ','),
+            brk = '}\n'
+          ) %>%
+          dplyr::select_at(c('index', 'tag', fldsel, 'brk')) %>%
+          dplyr::mutate_at(fldsel, ~paste0('={', ., '},')) %>%
+          tidyr::gather('var', 'val', -index) %>%
+          dplyr::mutate(
+            val = gsub('^\\=\\{NA\\},$', '={},', val),
+            val = dplyr::case_when(
+              !var %in% c('tag', 'brk') ~ paste0('\t', var, val),
+              T ~ val
+            ),
+            var = factor(var, levels = levs)
+          ) %>%
+          dplyr::arrange(index, var) %>%
+          dplyr::mutate_if(is.factor, as.character) %>%
+          dplyr::group_by(index) %>%
+          dplyr::mutate(subindex = letters[1:n()]) %>%
+          dplyr::ungroup()
+
+        return(tmp)
+
+      })
+    ) %>%
+    tidyr::unnest(data) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(index, subindex) %>%
+    dplyr::pull(val)
+
+  return(out)
+
+}
 
