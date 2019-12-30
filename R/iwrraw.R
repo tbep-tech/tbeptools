07b33cd35d80ed230a497775651dcb86507723fe
fawda123 <mbafs2012@gmail.com>
2019-12-30 10:02:35 -0500

    adding to example docs as dontrun to iwrraw and tidalcreeks for creation of rdata files from external sas data

--- R/iwrraw.R
+++ R/iwrraw.R
@@ -2,6 +2,17 @@
 #'
 #' Florida Department of Environmental Protection, Impaired Waters Rule, Run 56
 #'
+#' @details
+#' This file was created by importing a SAS file directly into R using the \code{\link[haven]{read_sas}} function.  The SAS file is prepared annually by FDEP for Janicki Environmental, Inc.
+#'
 #' @format A data frame 1224558 rows and 52 variables
 #' @family utilities
+#' @examples
+#' \dontrun{
+#' library(haven)
+#'
+#' iwrraw <- read_sas('../../02_DOCUMENTS/tidal_creeks/iwr56_tidalcreeks.sas7bdat')
+#' save(iwrraw, file = here('data', 'iwrraw.RData'), compress = 'xz')
+#'
+#' }
 "iwrraw"
