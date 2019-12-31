bfa6779cf92a3e15a75c7cba36b4f4917a138ef2
fawda123 <mbafs2012@gmail.com>
2019-12-31 13:18:39 -0500

    added tidaltargets data object

---
+++ R/tidaltargets.R
@@ -0,0 +1,13 @@
+#' Tidal creek nitrogen targets
+#'
+#' Tidal creek nitrogen targets
+#'
+#' @format A data frame with 2 rows and 4 variables:
+#' \describe{
+#'   \item{region}{chr}
+#'   \item{threshold}{num}
+#'   \item{action}{num}
+#'   \item{caution}{num}
+#' }
+#' @family utilities
+"tidaltargets"
---
+++ data-raw/tidaltargets-raw.R
@@ -0,0 +1,11 @@
+# create tidal creek nitrogen targets -------------------------------------
+
+tidaltargets <- data.frame(
+  region = c("West Central", "Peninsula"),
+  threshold = c(1.65, 1.54),
+  action = c(1.46, 1.36),
+  caution = c("1.46 - 0.0174 * (23.78 - (Creek_Length_m / 1000))", "1.36 - 0.0174 * (23.78 - (Creek_Length_m / 1000))"),
+  stringsAsFactors = FALSE
+)
+
+save(tidaltargets, file = 'data/tidaltargets.RData', compress = 'xz')
