8aebc7461b3cdc4363c9c3d58bb1039a531493a3
fawda123 <mbafs2012@gmail.com>
2020-01-07 11:25:58 -0500

    added tbshed object for watershed

--- R/tbseg.R
+++ R/tbseg.R
@@ -2,10 +2,14 @@
 #'
 #' Spatial data object of Tampa Bay segments
 #'
-#' @format A simple features \code{\link[sf]{sf}} object (POLYGON) with 4 feaures and 2 fields, +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
+#' @format A simple features \code{\link[sf]{sf}} object (POLYGON) with 4 features and 2 fields, +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
 #' \describe{
 #'   \item{long_name}{chr}
 #'   \item{bay_segment}{chr}
 #' }
 #' @family utilities
+#'
+#' @examples
+#' library(sf)
+#' plot(st_geometry(tbseg))
 "tbseg"
