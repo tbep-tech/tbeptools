c8892ecadcc9739755d5cf9bdc0e6e7e2410f06d
fawda123 <mbafs2012@gmail.com>
2020-01-12 10:00:19 -0500

    inner_join for show_tdlcrk

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,14 @@
-.onLoad <- function(libname, pkgname) {
-  # if (Sys.info()[1] == "Linux") {
-  #   dir.create('~/.fonts')
-  #   file.copy("inst/extdata/fonts/Lato-Light.ttf", "~/.fonts")
-  #   system('fc-cache -f ~/.fonts')
-  # }
-  if (Sys.info()[1] == "Windows") {
-    suppressMessages(extrafont::font_import(path = system.file('extdata/fonts', package = 'tbeptools'), pattern = "Lato-Light", prompt = FALSE))
-    extrafont::loadfonts(device = "win", quiet = TRUE)
-    extrafont::loadfonts(device = 'pdf', quiet = TRUE)
-    windowsFonts()
-  }
-  # print(extrafont::fonts())
-}
+# .onLoad <- function(libname, pkgname) {
+#   if (Sys.info()[1] == "Linux") {
+#     dir.create('~/.fonts')
+#     file.copy("inst/extdata/fonts/Lato-Light.ttf", "~/.fonts")
+#     system('fc-cache -f ~/.fonts')
+#   }
+#   if (Sys.info()[1] == "Windows") {
+#     suppressMessages(extrafont::font_import(path = system.file('extdata/fonts', package = 'tbeptools'), pattern = "Lato", prompt = FALSE))
+#     extrafont::loadfonts(device = "win", quiet = TRUE)
+#     extrafont::loadfonts(device = 'pdf', quiet = TRUE)
+#     windowsFonts()
+#   }
+# }
+#
