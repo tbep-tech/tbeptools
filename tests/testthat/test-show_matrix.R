030bb5def2965d738543fc3180c463dfe8a37d7a
fawda123 <mbafs2012@gmail.com>
2020-01-24 14:28:17 -0500

    updating matrix tests

--- tests/testthat/test-show_matrix.R
+++ tests/testthat/test-show_matrix.R
@@ -1,8 +1,8 @@
 test_that("Checking show_matrix ggplot class", {
-  result <- show_matrix(epcdata)
+  result <- show_matrix(epcdata, historic = TRUE, abbrev = TRUE)
   expect_is(result, 'ggplot')
 })
 test_that("Checking show_matrix reactable class", {
-  result <- show_matrix(epcdata, asreact = TRUE)
+  result <- show_matrix(epcdata, asreact = TRUE, historic = TRUE, abbrev = TRUE)
   expect_is(result, 'reactable')
 })
--- tests/testthat/test-show_wqmatrix.R
+++ tests/testthat/test-show_wqmatrix.R
@@ -1,9 +1,9 @@
 test_that("Checking show_wqmatrix ggplot class", {
-  result <- show_wqmatrix(epcdata)
+  result <- show_wqmatrix(epcdata, abbrev = TRUE)
   expect_is(result, 'ggplot')
 })
 test_that("Checking show_wqmatrix reactable class", {
-  result <- show_wqmatrix(epcdata, asreact = TRUE)
+  result <- show_wqmatrix(epcdata, asreact = TRUE, abbrev = TRUE)
   expect_is(result, 'reactable')
 })
 
