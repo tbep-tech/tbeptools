a91bec5c42c818e9b2568efc29e930fa0aa4dc01
fawda123 <mbafs2012@gmail.com>
2020-03-05 16:52:43 -0500

    nitrogen ratio indicator added to anlz_tdlcrkindic

--- R/anlz_tdlcrkindic.R
+++ R/anlz_tdlcrkindic.R
@@ -6,7 +6,7 @@
 #' @param iwrraw FDEP impaired waters rule run 56 data base as \code{\link{data.frame}}
 #' @param yr numeric for reference year to evaluate, scores are based on the planning period beginning ten years prior to this date
 #'
-#' @details Annual geometric means for additional water quality data available at each wbid, JEI combination.  Florida trophic state index values are also estimated where data are available.
+#' @details Annual geometric means for additional water quality data available at each wbid, JEI combination.  Florida trophic state index values are also estimated where data are available.  Nitrogen ratios are estimated for JEIs that cover source (upstream, freshwater) and tidal (downstream) WBIDs, defined as the ratio of concentrations between the two (i.e., ratios > 1 mean source has higher concentrations).  Nitrogen ratios for a given year reflect the median of all daily ratios when concentrations were measured in both a source and tidal segment during the same day.  Note that a ratio of one can be obtained if both the source and tidal segments are at minimum detection.
 #'
 #' @return A \code{\link{data.frame}} with the indicator values for each tidal creek
 #' @export
@@ -59,34 +59,42 @@ anlz_tdlcrkindic <- function(tidalcreeks, iwrraw, yr = 2018) {
     dplyr::filter(!is.na(year)) %>%
     dplyr::select(-Creek_Length_m)
 
-  # # nitrate ratio
-  # # done separately for each date and combined JEI
-  # nitdat <- iwrdat %>%
-  #   dplyr::filter(masterCode %in% 'NO23') %>%
-  #   dplyr::filter(!is.na(result)) %>%
-  #   dplyr::filter(JEI %in% unique(tidalcreeks$JEI)) %>%
-  #   dplyr::mutate(
-  #     result = exp(result),
-  #     class = dplyr::case_when(
-  #       class %in% c('3F', '1') ~ 'Source',
-  #       class %in% c('3M', '2') ~ 'Tidal'
-  #     )
-  #   ) %>%
-  #   dplyr::group_by(JEI, date, class) %>%
-  #   summarise(result = mean(result, na.rm = T)) %>%
-  #   dplyr::ungroup() %>%
-  #   tidyr::spread(class, result) %>%
-  #   dplyr::mutate(
-  #     no23_ratio = Source / Tidal,
-  #     year = strftime(date, '%Y'),
-  #     nit_mark = dplyr::case_when(
-  #       Tidal > 0.05 & no23_ratio < 1 ~ 1,
-  #       Tidal >= 0.05 & no23_ratio >= 1 ~ 0,
-  #       T ~ NaN
-  #     )
-  #   ) %>%
-  #   na.omit
+  # nitrate ratio
+  # done separately for each date and combined JEI
+  # annual estimates are median of daily values within a year
+  nitrat <- iwrdat %>%
+    dplyr::filter(masterCode %in% 'NO23') %>%
+    dplyr::filter(!is.na(result)) %>%
+    dplyr::filter(JEI %in% unique(tidalcreeks$JEI)) %>%
+    dplyr::mutate(
+      result = exp(result),
+      class = dplyr::case_when(
+        class %in% c('3F', '1') ~ 'Source',
+        class %in% c('3M', '2') ~ 'Tidal'
+      )
+    ) %>%
+    dplyr::group_by(JEI, date, class) %>%
+    summarise(result = mean(result, na.rm = T)) %>%
+    dplyr::ungroup() %>%
+    tidyr::spread(class, result) %>%
+    dplyr::mutate(
+      no23_ratio = Source / Tidal,
+      year = as.numeric(strftime(date, '%Y'))#,
+      # nit_mark = dplyr::case_when(
+      #   Tidal > 0.05 & no23_ratio < 1 ~ 1,
+      #   Tidal >= 0.05 & no23_ratio >= 1 ~ 0,
+      #   T ~ NaN
+      # )
+    ) %>%
+    na.omit %>%
+    dplyr::group_by(JEI, year) %>%
+    summarise(no23_ratio = median(no23_ratio, na.rm = T)) %>%
+    ungroup
 
-  return(alldat)
+  # join nitrogen ratio data to all other data by JEI (same value applies to JEI across wbids)
+  out <- alldat %>%
+    dplyr::full_join(nitrat, by = c('JEI', 'year'))
+
+  return(out)
 
 }
--- tests/testthat/test-anlz_tdlcrkindic.R
+++ tests/testthat/test-anlz_tdlcrkindic.R
@@ -3,15 +3,15 @@ test_that("Checking anlz_tdlcrkindic", {
   result <- anlz_tdlcrkindic(tidalcreeks, iwrraw, yr = 2018) %>%
     .[nrow(.), ]
 
-  expect_equal(result, structure(list(id = 564L, wbid = "2026", JEI = "SC37", name = "",
-                                      class = "3M", year = 2008, CHLAC = 2.80886253685455, COLOR = NA_real_,
-                                      COND = 25361.95004897, DO = 2.19875339132418, DOSAT = 27.4890362838677,
-                                      NO23 = 0.0259680358062411, ORGN = NA_real_, SALIN = 15.4206629191757,
-                                      TKN = 0.659814567759305, TN = 0.693137546997186, TP = 0.0304139957907973,
-                                      TSS = 4.58114078563817, TURB = 1.34968226305967, chla_tn_ratio = 4.05238837374072,
-                                      tn_tp_ratio = 22.7900849255367, chla_tsi = 31.6720263874652,
-                                      tn_tsi = 48.742768984365, tn2_tsi = 51.7227750058298, tp_tsi = 45.1171937599776,
-                                      tp2_tsi = 56.7917082115845, nut_tsi = 46.9299813721713, tsi = 39.3010038798182), row.names = 1025L, class = "data.frame"))
+  expect_equal(result, structure(list(id = NA_integer_, wbid = NA_character_, JEI = "LC02",
+                                      name = NA_character_, class = NA_character_, year = 2014,
+                                      CHLAC = NA_real_, COLOR = NA_real_, COND = NA_real_, DO = NA_real_,
+                                      DOSAT = NA_real_, NO23 = NA_real_, ORGN = NA_real_, SALIN = NA_real_,
+                                      TKN = NA_real_, TN = NA_real_, TP = NA_real_, TSS = NA_real_,
+                                      TURB = NA_real_, chla_tn_ratio = NA_real_, tn_tp_ratio = NA_real_,
+                                      chla_tsi = NA_real_, tn_tsi = NA_real_, tn2_tsi = NA_real_,
+                                      tp_tsi = NA_real_, tp2_tsi = NA_real_, nut_tsi = NA_real_,
+                                      tsi = NA_real_, no23_ratio = 1.84210526315789), row.names = 1027L, class = "data.frame"))
 
 })
 
