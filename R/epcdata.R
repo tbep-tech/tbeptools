da88e947901459121fe64f8122b081d020f9635b
fawda123 <mbafs2012@gmail.com>
2020-04-01 15:11:11 -0400

    read_formwq now includes qaqc columns for secchi and chl

--- R/epcdata.R
+++ R/epcdata.R
@@ -2,7 +2,7 @@
 #'
 #' All bay data as of 04012020
 #'
-#' @format A data frame with 25669 rows and 15 variables:
+#' @format A data frame with 25669 rows and 17 variables:
 #' \describe{
 #'   \item{bay_segment}{chr}
 #'   \item{epchc_station}{num}
@@ -15,7 +15,9 @@
 #'   \item{Sample_Depth_m}{num}
 #'   \item{sd_m}{num}
 #'   \item{sd_raw_m}{num}
+#'   \item{sq_q}{chr}
 #'   \item{chla}{num}
+#'   \item{chla_q}{chr}
 #'   \item{Sal_Top_ppth}{num}
 #'   \item{Sal_Mid_ppth}{num}
 #'   \item{Sal_Bottom_ppth}{num}
--- tests/testthat/test-read_formwq.R
+++ tests/testthat/test-read_formwq.R
@@ -51,6 +51,6 @@ test_that("Checking read_formwq", {
 
   # check if number of columns is equal to 11
   result <- ncol(dat)
-  expect_equal(result, 15)
+  expect_equal(result, 17)
 
 })
