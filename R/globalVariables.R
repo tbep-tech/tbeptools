6f5dcd3a987729c481c95654a4114a6ec8634614
fawda123 <mbafs2012@gmail.com>
2020-04-24 17:04:48 -0400

    intro vignette updated with tbni functions, placeholder vignette for tbni, site rebuild

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -20,7 +20,8 @@ globalVariables(c("Chlorophyll_a uncorr_Q", "Latitude", "Longitude", "SampleTime
                   "Selected_Taxa", "Shannon", "Simpson", "TGAbund", "TGTaxa", "TSAbund", "TSTaxa", "Taxa",
                   "Total_N", "Year", "lnpi", "pi2", "pilnpi", "sumpi2", "sumpilnpi", "Chlorophyll_a uncorr_ugL",
                   "NumGuilds", "NumTaxa", "ScoreBenthicTaxa", "ScoreNumGuilds", "ScoreNumTaxa", "ScoreShannon",
-                  "ScoreTaxaSelect", "TBNI_Score", "TaxaSelect", "fimdat", "tbniref", "Segment_TBNI"))
+                  "ScoreTaxaSelect", "TBNI_Score", "TaxaSelect", "fimdat", "tbniref", "Segment_TBNI",
+                  "segment_col"))
 
 #' @importFrom grDevices rgb
 NULL
