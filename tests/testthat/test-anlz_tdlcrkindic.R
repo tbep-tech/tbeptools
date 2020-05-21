b11849be9661f1ba4223cd00df271af3846d6485
fawda123 <mbafs2012@gmail.com>
2020-05-21 08:12:37 -0400

    fixing test for anlz_tdlcrkindic

--- tests/testthat/test-anlz_tdlcrkindic.R
+++ tests/testthat/test-anlz_tdlcrkindic.R
@@ -3,15 +3,14 @@ test_that("Checking anlz_tdlcrkindic", {
   result <- anlz_tdlcrkindic(tidalcreeks, iwrraw, yr = 2018) %>%
     .[nrow(.), ]
 
-  expect_equal(result, structure(list(id = NA_integer_, wbid = NA_character_, JEI = "LC02",
-                                      name = NA_character_, class = NA_character_, year = 2014,
-                                      CHLAC = NA_real_, COLOR = NA_real_, COND = NA_real_, DO = NA_real_,
-                                      DOSAT = NA_real_, NO23 = NA_real_, ORGN = NA_real_, SALIN = NA_real_,
-                                      TKN = NA_real_, TN = NA_real_, TP = NA_real_, TSS = NA_real_,
-                                      TURB = NA_real_, chla_tn_ratio = NA_real_, tn_tp_ratio = NA_real_,
-                                      chla_tsi = NA_real_, tn_tsi = NA_real_, tn2_tsi = NA_real_,
-                                      tp_tsi = NA_real_, tp2_tsi = NA_real_, nut_tsi = NA_real_,
-                                      tsi = NA_real_, no23_ratio = 1.84210526315789), row.names = 1027L, class = "data.frame"))
+  expect_equal(result, structure(list(id = 572L, wbid = "1976", JEI = "SC40", name = "Big Slough Canal",
+                                      class = "1", year = 2008, CHLAC = NA_real_, COLOR = NA_real_,
+                                      COND = 459, DO = NA_real_, DOSAT = NA_real_, NO23 = NA_real_,
+                                      ORGN = NA_real_, SALIN = NA_real_, TKN = NA_real_, TN = NA_real_,
+                                      TP = NA_real_, TSS = NA_real_, TURB = NA_real_, chla_tn_ratio = NA_real_,
+                                      tn_tp_ratio = NA_real_, chla_tsi = NA_real_, tn_tsi = NA_real_,
+                                      tn2_tsi = NA_real_, tp_tsi = NA_real_, tp2_tsi = NA_real_,
+                                      nut_tsi = NaN, tsi = NaN, no23_ratio = NA_real_), row.names = 1180L, class = "data.frame"))
 
 })
 
