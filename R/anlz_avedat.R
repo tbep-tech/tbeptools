54bae4055ca7f32b5b9053d20ca64ae3cfb6b017
fawda123 <mbafs2012@gmail.com>
2020-08-09 15:03:25 -0400

    simplified anlz_avedat

--- R/anlz_avedat.R
+++ R/anlz_avedat.R
@@ -3,6 +3,9 @@
 #' Estimate annual means for chlorophyll and secchi data
 #'
 #' @param epcdata \code{data.frame} formatted from \code{read_importwq}
+#' @param yrrng numeric vector indicating min, max years to include
+#' @param partialyr logical indicating if incomplete annual data for the most recent year are approximated by five year monthly averages for each parameter
+#'
 #'
 #' @return Mean estimates for chlorophyll and secchi
 #'
@@ -16,158 +19,71 @@
 #' @examples
 #' # view average estimates
 #' anlz_avedat(epcdata)
-anlz_avedat <- function(epcdata){
+anlz_avedat <- function(epcdata, yrrng, partialyr){
 
-  # remove MTB for initial calc
-  tbdata <- epcdata %>%
-    dplyr::filter(!bay_segment %in% 'MTB')
-
-  # chlorophyll monthly averages
-  tbmonchla <- tbdata %>%
-    select(yr, mo, bay_segment, chla) %>%
-    drop_na() %>%
-    group_by(yr, mo, bay_segment) %>%
-    summarise(mean_chla = mean(chla)) %>%
-    ungroup
-
-  # chlorophyll annual values
-  tbyrchla <- tbmonchla %>%
-    select(bay_segment, yr, mean_chla) %>%
-    drop_na() %>%
-    group_by(bay_segment, yr) %>%
-    summarise(mean_chla = mean(mean_chla)) %>%
-    ungroup
-
-  # monthly secchi averages
-  tbmonsdm <- tbdata %>%
-    select(yr, mo, bay_segment, sd_m) %>%
-    drop_na() %>%
-    group_by(yr, mo, bay_segment) %>%
-    summarise(mean_sdm = mean(sd_m)) %>%
-    ungroup
-
-  # annual secchi averages
-  tbyrsdm <- tbmonsdm %>%
-    select(bay_segment, yr, mean_sdm) %>%
-    drop_na() %>%
-    group_by(bay_segment, yr) %>%
-    summarise(mean_sdm = mean(mean_sdm)) %>%
-    ungroup
-
-  # process MTB data using weighted averages of 3 subsegments
-  mtbdata <- epcdata %>%
-    dplyr::filter(bay_segment %in% "MTB") %>%
-    mutate(
-      bay_segment = case_when(
+  # year month averages
+  # long format, separate bay_segment for MTB into sub segs
+  # mtb year month averages are weighted
+  moout <- epcdata %>%
+    dplyr::select(yr, mo, bay_segment, epchc_station, chla, sd_m) %>%
+    tidyr::gather('var', 'val', chla, sd_m) %>%
+    dplyr::mutate(
+      bay_segment = dplyr::case_when(
         epchc_station %in% c(9, 11, 81, 84) ~ "MT1",
         epchc_station %in% c(13, 14, 32, 33) ~ "MT2",
-        epchc_station %in% c(16, 19, 28, 82) ~ "MT3"
-        )
-      )
-
-  # mtb monthly chlorophyll averages
-  mtbmonchla <- mtbdata %>%
-    select(yr, mo, bay_segment, chla) %>%
-    drop_na() %>%
-    group_by(yr, mo, bay_segment) %>%
-    summarise(mean_chla = mean(chla)) %>%
-    ungroup %>%
-    mutate(
-      chla = case_when(
-        bay_segment %in% "MT1" ~ mean_chla * 2108.7,
-        bay_segment %in% "MT2" ~ mean_chla * 1041.9,
-        bay_segment %in% "MT3" ~ mean_chla * 974.6
-      )
-    )
-
-  # mtb monthly secchi averages
-  mtbmonsdm <- mtbdata %>%
-    select(yr, mo, bay_segment, sd_m) %>%
-    drop_na() %>%
-    group_by(yr, mo, bay_segment) %>%
-    summarise(mean_sd_m = mean(sd_m)) %>%
-    ungroup %>%
-    mutate(
-      sdm = case_when(
-        bay_segment %in% "MT1" ~ mean_sd_m * 2108.7,
-        bay_segment %in% "MT2" ~ mean_sd_m * 1041.9,
-        bay_segment %in% "MT3" ~ mean_sd_m * 974.6
-      )
-    )
-
-  # mtb weighted average monthly chlorophyll
-  mtbmoyrchla <- mtbmonchla %>%
-    select(yr, mo, bay_segment, chla) %>%
-    drop_na() %>%
-    group_by(yr, mo) %>%
-    summarise(sum_chla = sum(chla)) %>%
-    ungroup %>%
-    mutate(
-      mean_chla = sum_chla / 4125.2,
-      bay_segment = "MTB"
-      )
-
-  # mtb average of monthly area weighted chlorophyll
-  mtbyrchla <- mtbmoyrchla %>%
-    select(yr, mean_chla) %>%
-    drop_na() %>%
-    group_by(yr) %>%
-    summarise(mean_chla = mean(mean_chla)) %>%
-    ungroup %>%
-    mutate(bay_segment = "MTB")
-
-  # mtb weighted average of secchi
-  mtbmoyrsdm <- mtbmonsdm %>%
-    select(yr, mo, bay_segment, sdm) %>%
-    drop_na() %>%
-    group_by(yr, mo) %>%
-    summarise(sum_sdm = sum(sdm)) %>%
-    ungroup %>%
-    mutate(
-      mean_sdm = sum_sdm / 4125.2,
-      bay_segment = "MTB"
-      )
-
-  mtbyrsdm <- mtbmoyrsdm %>%
-    select(yr, mean_sdm) %>%
-    drop_na() %>%
-    group_by(yr) %>%
-    summarise(mean_sdm = mean(mean_sdm)) %>%
-    ungroup %>%
-    mutate(bay_segment = "MTB")
-
-  # combine mtb ests with others, monthly
-  chlamodata <- bind_rows(tbmonchla, mtbmoyrchla) %>%
-    gather('var', 'val', -yr, -mo, -bay_segment) %>%
-    dplyr::filter(!var %in% 'sum_chla')
-  sdmmodata <- bind_rows(tbmonsdm, mtbmoyrsdm) %>%
-    dplyr::mutate(
-      mean_la = case_when(
-        bay_segment %in% "OTB" ~ 1.49 / mean_sdm,
-        bay_segment %in% "HB" ~ 1.61 / mean_sdm,
-        bay_segment %in% "MTB" ~ 1.49 / mean_sdm,
-        bay_segment %in% "LTB" ~ 1.84 / mean_sdm
+        epchc_station %in% c(16, 19, 28, 82) ~ "MT3",
+        TRUE ~ bay_segment
       )
     ) %>%
-    gather('var', 'val', -yr, -mo, -bay_segment) %>%
-    dplyr::filter(!var %in% c('mean_sdm', 'sum_sdm'))
-  moout <- bind_rows(chlamodata, sdmmodata)
+    dplyr::group_by(bay_segment, yr, mo, var) %>%
+    dplyr::summarise(val = mean(val, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      val = dplyr::case_when(
+        bay_segment %in% "MT1" ~ val * 2108.7,
+        bay_segment %in% "MT2" ~ val * 1041.9,
+        bay_segment %in% "MT3" ~ val * 974.6,
+        TRUE ~ val
+        ),
+      bay_segment = dplyr::case_when(
+        bay_segment %in% c('MT1', 'MT2', 'MT3') ~ 'MTB',
+        TRUE ~ bay_segment
+      )
+    ) %>%
+    dplyr::group_by(bay_segment, yr, mo, var) %>%
+    dplyr::summarise(
+      val = sum(val, na.rm = T)
+    ) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      val = dplyr::case_when(
+        bay_segment %in% 'MTB' ~ val / 4125.2,
+        TRUE ~ val
+      ),
+      var = dplyr::case_when(
+        var == 'chla' ~ 'mean_chla',
+        var == 'sd_m' ~ 'mean_la'
+      ),
+      val = dplyr::case_when(
+        bay_segment %in% "OTB" & var == 'mean_la' ~ 1.49 / val,
+        bay_segment %in% "HB" & var == 'mean_la' ~ 1.61 / val,
+        bay_segment %in% "MTB" & var == 'mean_la' ~ 1.49 / val,
+        bay_segment %in% "LTB" & var == 'mean_la' ~ 1.84 / val,
+        TRUE ~ val
+      )
+    ) %>%
+    dplyr::filter(!is.na(val)) %>%
+    dplyr::filter(!is.infinite(val)) %>%
+    dplyr::arrange(var, yr, mo, bay_segment)
 
-  # combine mtb ests with others, annual
-  # geisson factors for secchi depth for % light availability
-  sdmdata <- bind_rows(tbyrsdm, mtbyrsdm) %>%
-    mutate(
-      mean_la = case_when(
-        bay_segment %in% "OTB" ~ 1.49 / mean_sdm,
-        bay_segment %in% "HB" ~ 1.61 / mean_sdm,
-        bay_segment %in% "MTB" ~ 1.49 / mean_sdm,
-        bay_segment %in% "LTB" ~ 1.84 / mean_sdm
-        )
-      ) %>%
-    gather('var', 'val', -yr, -bay_segment)
-  chladata <- bind_rows(tbyrchla, mtbyrchla) %>%
-    gather('var', 'val', -yr, -bay_segment)
-  anout <- bind_rows(chladata, sdmdata)
+  # annual data
+  anout <- moout %>%
+    dplyr::group_by(yr, bay_segment, var) %>%
+    dplyr::summarise(val = mean(val, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!is.na(val)) %>%
+    dplyr::filter(!is.infinite(val)) %>%
+    dplyr::arrange(var, yr, bay_segment)
 
   # combine all
   out <- list(
--- tests/testthat/test-anlz_avedat.R
+++ tests/testthat/test-anlz_avedat.R
@@ -2,14 +2,15 @@ test_that("Checking anlz_avedat months", {
   result <- anlz_avedat(epcdata) %>%
     .[['mos']] %>%
     pull(val) %>%
-    .[1:4]
-  expect_equal(c(36.2, 1.75, 4.4, 42.4), result)
+    .[(length(.) - 4):length(.)]
+  expect_equal(c(0.488524590163934, 0.914772727272727, 0.471794871794872, 0.442102969064063,
+                 0.691785714285714), result)
 })
 test_that("Checking anlz_avedat years", {
   result <- anlz_avedat(epcdata) %>%
     .[['ann']] %>%
     pull(val) %>%
-    .[1:4] %>%
-    round(5)
-  expect_equal(c(22.40455, 27.93182, 29.45111, 32.54222), result)
+    .[(length(.) - 4):length(.)]
+  expect_equal(c(0.78363096716534, 0.878181818181818, 0.659083139083139, 0.482318533979087,
+                 0.588208009979948), result)
 })
