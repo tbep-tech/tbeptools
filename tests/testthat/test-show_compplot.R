d6868c743967938ba63203a395b59d4bd31a4410
fawda123 <mbafs2012@gmail.com>
2020-09-22 17:35:56 -0400

    fixing show_compplot tests

--- tests/testthat/test-show_compplot.R
+++ tests/testthat/test-show_compplot.R
@@ -1,13 +1,12 @@
 test_that("Checking show_compplot", {
 
   transect <- read_transect(training = T)
-  dat<- anlz_transect(transect, training = T)
 
-  result <- show_compplot(dat, site = '1')
+  result <- show_compplot(transect, site = '1')
 
   expect_is(result, 'ggplot')
 
-  result <- show_compplot(dat, site = '1', varplo = 'Blade Length')
+  result <- show_compplot(transect, site = '1', varplo = 'Blade Length')
 
   expect_is(result, 'ggplot')
 
