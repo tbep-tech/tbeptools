64e61ec568ac103037ba4ca7e0a7b8b34aa3798f
fawda123 <mbafs2012@gmail.com>
2020-09-25 17:29:32 -0400

    push for trnpts generation

--- R/trnpts.R
+++ R/trnpts.R
@@ -7,11 +7,14 @@
 #' \dontrun{
 #' library(sf)
 #' library(dplyr)
+#' library(tbeptools)
 #'
 #' trnpts <- st_read('T:/05_GIS/SEAGRASS_TRANSECTS/TransectBasics2019.shp') %>%
 #'    st_transform(crs = 4326) %>%
 #'    dplyr::rename(MonAgency = 'MON_AGENCY') %>%
-#'    dplyr::filter(!as.character(TRAN_ID) %in% c('S8T1', 'S8T2', 'S8T3', 'S3T2', 'S4T12'))
+#'    dplyr::filter(!as.character(TRAN_ID) %in% c('S8T1', 'S8T2', 'S8T3', 'S3T2', 'S4T12')) %>%
+#'    sf::st_intersection(sf::st_make_valid(tbsegshed)) %>%
+#'    dplyr::selet(-long_name)
 #'
 #' save(trnpts, file = 'data/trnpts.RData', compress = 'xz')
 #' }
