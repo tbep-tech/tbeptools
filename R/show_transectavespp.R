02a7b8d8a96e164992f5d4c9e46b42c86ee67a82
fawda123 <mbafs2012@gmail.com>
2020-09-30 10:40:52 -0400

    added asreact argument to show_transectavespp, vignette update

--- R/show_transectavespp.R
+++ R/show_transectavespp.R
@@ -4,20 +4,23 @@
 #' @param bay_segment chr string for the bay segment, one to many of "OTB", "HB", "MTB", "LTB", "BCB"
 #' @param yrrng numeric indicating year ranges to evaluate
 #' @param species chr string of species to summarize, one to many of "Halodule", "Syringodium", "Thalassia", "Ruppia", "Halophila spp.", "Caulerpa spp."
-#' @param total logical indicating if total frequency occurrence for all species is also returned
+#' @param total logical indicating if total frequency occurrence for all species is also returned, only applies if \code{asreact = FALSE}
 #' @param alph numeric indicating alpha value for score category colors
 #' @param family optional chr string indicating font family for text labels
 #' @param plotly logical if matrix is created using plotly
+#' @param asreact logical if a reactable table is returned instead of a plot
 #'
 #' @details Results are based on averages across species by date and transect in each bay segment
 #'
-#' @return A \code{\link[ggplot2]{ggplot}} object showing trends over time by speies for selected bay segments if \code{plotly = FALSE}, otherwise a \code{\link[plotly]{plotly}} object
+#' @return If \code{asreact = F}, a \code{\link[ggplot2]{ggplot}} or \code{\link[plotly]{plotly}} (if \code{plotly = T}) object is returned showing trends over time by species for selected bay segments.  If \code{asreact = T}, a \code{\link[reactable]{reactable}} table showing results by year, segment, and species is returned.
 #' @export
 #'
 #' @family visualize
 #'
 #' @references
-#' This plot is a representation of Figure 2 in R. Johansson (2016) Seagrass Transect Monitoring in Tampa Bay: A Summary of Findings from 1997 through 2015, Technical report #08-16, Tampa Bay Estuary Program, St. Petersburg, Florida.
+#' The plot is a representation of figure 2 in Johansson, R. (2016) Seagrass Transect Monitoring in Tampa Bay: A Summary of Findings from 1997 through 2015, Technical report #08-16, Tampa Bay Estuary Program, St. Petersburg, Florida.
+#'
+#' The table is a representation of table 2, p. 163 in Yarbro, L. A., and P. R. Carlson, Jr., eds. 2016. Seagrass Integrated Mapping and Monitoring Program: Mapping and Monitoring Report No. 2. Fish and Wildlife Research Institute Technical Report TR-17 version 2. vi + 281 p.
 #'
 #' @importFrom magrittr "%>%"
 #'
@@ -29,34 +32,75 @@
 #' show_transectavespp(transectocc)
 show_transectavespp <- function(transectocc, bay_segment = c('OTB', 'HB', 'MTB', 'LTB', 'BCB'), yrrng = c(1998, 2019),
                                 species = c('Halodule', 'Syringodium', 'Thalassia', 'Ruppia', 'Halophila spp.', 'Caulerpa spp.'),
-                                total = TRUE, alph = 1, family = NA, plotly = FALSE){
+                                total = TRUE, alph = 1, family = NA, plotly = FALSE, asreact = FALSE){
 
-  # annual average by segment
-  toplo <- anlz_transectavespp(transectocc, total = total, bay_segment = bay_segment, yrrng = yrrng, species = species, by_seg = F)
+  # make plot
+  if(!asreact){
 
-  # sort color palette so its the same regardless of species selected
-  sppcol <- c('#FFFFFF', '#ED90A4', '#CCA65A', '#7EBA68', '#00C1B2', '#6FB1E7', '#D494E1')
-  names(sppcol) <- c('total', 'Halodule', 'Syringodium', 'Thalassia', 'Ruppia', 'Halophila spp.', 'Caulerpa spp.')
-  sppcol <- sppcol[levels(toplo$Savspecies)]
+    # annual average by segment
+    toplo <- anlz_transectavespp(transectocc, total = total, bay_segment = bay_segment, yrrng = yrrng, species = species, by_seg = F)
 
-  p <- ggplot2::ggplot(toplo, ggplot2::aes(x = yr, y = 100 * foest, fill = Savspecies)) +
-    ggplot2::geom_line(alpha = alph) +
-    ggplot2::geom_point(pch = 21, size = 3, alpha = alph) +
-    ggplot2::scale_fill_manual(values = sppcol) +
-    ggplot2::theme_bw() +
-    ggplot2::theme(
-      axis.title.x = ggplot2::element_blank(),
-      legend.title = ggplot2::element_blank(),
-      text = ggplot2::element_text(family = family)
-    ) +
-    ggplot2::labs(
-      y = '% frequency occurrence',
-      title = paste('Seagress estimates for', paste(bay_segment, collapse = ', '))
+    # sort color palette so its the same regardless of species selected
+    sppcol <- c('#FFFFFF', '#ED90A4', '#CCA65A', '#7EBA68', '#00C1B2', '#6FB1E7', '#D494E1')
+    names(sppcol) <- c('total', 'Halodule', 'Syringodium', 'Thalassia', 'Ruppia', 'Halophila spp.', 'Caulerpa spp.')
+    sppcol <- sppcol[levels(toplo$Savspecies)]
+
+    p <- ggplot2::ggplot(toplo, ggplot2::aes(x = yr, y = 100 * foest, fill = Savspecies)) +
+      ggplot2::geom_line(alpha = alph) +
+      ggplot2::geom_point(pch = 21, size = 3, alpha = alph) +
+      ggplot2::scale_fill_manual(values = sppcol) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(
+        axis.title.x = ggplot2::element_blank(),
+        legend.title = ggplot2::element_blank(),
+        text = ggplot2::element_text(family = family)
+      ) +
+      ggplot2::labs(
+        y = '% frequency occurrence',
+        title = paste('Seagress estimates for', paste(bay_segment, collapse = ', '))
+      )
+
+    if(plotly)
+      p <- plotly::ggplotly(p)
+
+    out <- p
+
+  }
+
+  # table
+  if(asreact){
+
+    # annual average by segment
+    totab <- anlz_transectavespp(transectocc, total = total, bay_segment = bay_segment, yrrng = yrrng, species = species, by_seg = T) %>%
+      dplyr::mutate(foest = 100 * foest) %>%
+      tidyr::spread(Savspecies, foest) %>%
+      dplyr::arrange(yr, bay_segment)
+
+    out <- reactable::reactable(totab,
+      columns = list(
+        yr = reactable::colDef(
+          name = 'Year',
+          format = reactable::colFormat(digits = 0)
+          ),
+        bay_segment = reactable::colDef(name = 'Bay segment'),
+        nsites = reactable::colDef(
+          name = '# of quadrats',
+          format = reactable::colFormat(digits = 0)
+          )
+      ),
+      groupBy = 'bay_segment',
+      defaultExpanded = T,
+      defaultPageSize = nrow(totab),
+      defaultColDef = reactable::colDef(
+        footerStyle = list(fontWeight = "bold"),
+        format = reactable::colFormat(digits = 1, separators = F),
+        resizable = TRUE
+      ),
+      filterable = T
     )
 
-  if(plotly)
-    p <- plotly::ggplotly(p)
+  }
 
-  return(p)
+  return(out)
 
 }
--- tests/testthat/test-show_transectavespp.R
+++ tests/testthat/test-show_transectavespp.R
@@ -9,3 +9,8 @@ test_that("Checking show_transectavespp plotly class", {
   result <- show_transectavespp(transectocc, plotly = T)
   expect_is(result, 'plotly')
 })
+
+test_that("Checking show_transectavespp reactable class", {
+  result <- show_transectavespp(transectocc, asreact = T)
+  expect_is(result, 'reactable')
+})
