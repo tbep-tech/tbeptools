2a3b76e43fa6221123151bd05c6fe731bf4c78aa
fawda123 <mbafs2012@gmail.com>
2020-10-20 17:41:48 -0400

    updating global variables [ci skip]

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -32,7 +32,8 @@ globalVariables(c("Chlorophyll_a uncorr_Q", "Latitude", "Longitude", "SampleTime
                   'SpeciesRichness', 'SpionidAbundance', 'StandardizingConstant', 'StandPropLnSpecies',
                   'StationID', 'StratumID', 'SumofAdjCount', 'SumofCount', 'TAXA_GROUP', 'TAXA_ID', 'TaxaCount',
                   'TaxaCountID', 'TaxaID', 'TaxaListId', 'TaxaListID', 'TBBI', 'TBBICat', 'tbsegshed',
-                  'TotalAbundance', 'bb', 'pa', 'TRAN_ID', 'bbest', 'focat', 'nsites', 'trnpts', 'foest'))
+                  'TotalAbundance', 'bb', 'pa', 'TRAN_ID', 'bbest', 'focat', 'nsites', 'trnpts', 'foest',
+                  'All', 'All (wt)', 'cntwts', 'grndn', 'nwts', 'per', 'tbbicatid', 'tbbicatwt', 'total'))
 
 #' @importFrom grDevices rgb
 NULL
