9619407b0318e71d8f6c90d7fdf77ee615d9921b
fawda123 <mbafs2012@gmail.com>
2020-11-13 16:45:04 -0500

    fixing test for anlz_transectave

--- tests/testthat/test-anlz_transectave.R
+++ tests/testthat/test-anlz_transectave.R
@@ -5,7 +5,7 @@ test_that("Checking results for anlz_transectave", {
   results <- anlz_transectave(transectocc)
   results <- results[1, ]
   expect_equal(results, structure(list(yr = 1998, bay_segment = structure(1L, .Label = c("Tampa Bay",
-                                                                                         "OTB", "HB", "MTB", "LTB", "BCB"), class = "factor"), foest = 69.3889047351134,
+                                                                                         "OTB", "HB", "MTB", "LTB", "BCB"), class = "factor"), foest = 69.5422696525089,
                                        focat = structure(3L, .Label = c("red", "orange", "yellow",
                                                                         "green"), class = "factor")), row.names = c(NA, -1L), class = c("tbl_df",
                                                                                                                                         "tbl", "data.frame")))
