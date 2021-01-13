57f5c9b9c1f6bd6e5a1b947f1c102b97fcf448c7
fawda123 <mbafs2012@gmail.com>
2021-01-13 09:14:41 -0500

    update tidaltargets object with 1.38, 1.30 for investigate/caution threshold

--- data-raw/tidaltargets-raw.R
+++ data-raw/tidaltargets-raw.R
@@ -3,8 +3,8 @@
 tidaltargets <- data.frame(
   region = c("West Central", "Peninsula"),
   prioritize = c(1.65, 1.54),
-  investigate = c(1.46, 1.36),
-  caution = c("1.46 - 0.0174 * (23.78 - (Creek_Length_m / 1000))", "1.36 - 0.0174 * (23.78 - (Creek_Length_m / 1000))"),
+  investigate = c(1.38, 1.30),
+  caution = c("1.38 - 0.0174 * (23.78 - (Creek_Length_m / 1000))", "1.30 - 0.0174 * (23.78 - (Creek_Length_m / 1000))"),
   stringsAsFactors = FALSE
 )
 
