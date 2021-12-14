418dd7fff09006d9a85697c0100e4762f33a7a5a
fawda123 <mbafs2012@gmail.com>
2021-12-14 09:43:43 -0500

    fixing anlz_tbbimed tests

--- tests/testthat/test-anlz_tbbimed.R
+++ tests/testthat/test-anlz_tbbimed.R
@@ -2,7 +2,7 @@ test_that("Checking results for anlz_tbbimed", {
 
   results <- anlz_tbbimed(tbbiscr)
   results <- results[1, ]
-  expect_equal(results, structure(list(bay_segment = structure(8L, .Label = c("HB", "OTB",
+  expect_equal(results, structure(list(bay_segment = structure(8L, .Label = c("OTB", "HB",
                                                                               "MTB", "LTB", "TCB", "MR", "BCB", "All", "All (wt)"), class = "factor"),
                                        yr = 1993, Degraded = 0.3, Healthy = 0.244444444444444, Intermediate = 0.455555555555556,
                                        TBBICat = structure(1L, .Label = c("Poor", "Fair", "Good"
@@ -18,7 +18,7 @@ test_that("Checking results for anlz_tbbimed, rev = T", {
   results <- anlz_tbbimed(tbbiscr, rev = T)
   results <- results[1, ]
   expect_equal(results, structure(list(bay_segment = structure(2L, .Label = c("All (wt)",
-                                                                              "All", "BCB", "MR", "TCB", "LTB", "MTB", "OTB", "HB"), class = "factor"),
+                                                                              "All", "BCB", "MR", "TCB", "LTB", "MTB", "HB", "OTB"), class = "factor"),
                                        yr = 1993, Degraded = 0.3, Healthy = 0.244444444444444, Intermediate = 0.455555555555556,
                                        TBBICat = structure(1L, .Label = c("Poor", "Fair", "Good"
                                        ), class = "factor")), row.names = c(NA, -1L), groups = structure(list(
