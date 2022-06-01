afee1e33bb0d2953469130b797c653afef6871db
fawda123 <mbafs2012@gmail.com>
2022-06-01 09:09:11 -0400

    update benthic data using new path, change some fig dims in tbbi vignette

--- R/benthicdata.R
+++ R/benthicdata.R
@@ -1,6 +1,6 @@
-#' Benthic data for the Tampa Bay Benthic Index current as of 08012021
+#' Benthic data for the Tampa Bay Benthic Index current as of 06012022
 #'
-#' Benthic data for the Tampa Bay Benthic Index current as of 08012021
+#' Benthic data for the Tampa Bay Benthic Index current as of 06012022
 #'
 #' @format A nested \code{\link[tibble]{tibble}} with 3 rows and 2 variables:
 #' \describe{
