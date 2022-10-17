d76ca19ca8b152d6851f088c0760990f03d18de3
fawda123 <mbafs2012@gmail.com>
2022-10-17 14:09:52 -0400

    fixing read_formwq for different column names

--- R/read_formwq.R
+++ R/read_formwq.R
@@ -29,24 +29,24 @@ read_formwq <- function(datin){
   # format
   out <- datin %>%
     dplyr::mutate(
-      epchc_station = as.numeric(Station_Number),
-      sd_m = as.numeric(Secchi_Depth_m),
-      sd_check = as.numeric((Total_Depth_m*3.2809)-(Secchi_Depth_m*3.2809)),
+      epchc_station = as.numeric(StationNumber),
+      sd_m = as.numeric(SecchiDepth),
+      sd_check = as.numeric((TotalDepth*3.2809)-(SecchiDepth*3.2809)),
       sd_m = dplyr::case_when(
         Secchi_Q == ">" ~ NaN,
         sd_check < 0.5 ~ NaN,
         T ~ sd_m
         ),
-      chla = suppressWarnings(as.numeric(`Chlorophyll_a uncorr_ugL`)),
-      tn = suppressWarnings(as.numeric(`Total_Nitrogen_mgL`)),
+      chla = suppressWarnings(as.numeric(`Chlorophyll_a`)),
+      tn = suppressWarnings(as.numeric(`Total_Nitrogen`)),
       yr = lubridate::year(SampleTime),
       mo = lubridate::month(SampleTime)
       ) %>%
     dplyr::inner_join(stations, by = 'epchc_station') %>%
     dplyr::select(bay_segment, epchc_station, SampleTime, yr, mo, Latitude, Longitude, SampleTime,
-                  Total_Depth_m, Sample_Depth_m, tn, tn_q = `Total_Nitrogen_Q`, sd_m, sd_raw_m = Secchi_Depth_m, sd_q = Secchi_Q, chla, chla_q = `Chlorophyll_a uncorr_Q`, Sal_Top_ppth, Sal_Mid_ppth,
-                  Sal_Bottom_ppth, Temp_Water_Top_degC, Temp_Water_Mid_degC, Temp_Water_Bottom_degC, `Turbidity_JTU-NTU`, Turbidity_Q,
-                  Color_345_F45_PCU, Color_345_F45_Q
+                  Total_Depth_m = TotalDepth, Sample_Depth_m = SampleDepth, tn, tn_q = `Total_NitrogenQ`, sd_m, sd_raw_m = SecchiDepth, sd_q = Secchi_Q, chla, chla_q = `Chlorophyll_aQ`, Sal_Top_ppth = `Sal-T`, Sal_Mid_ppth = `Sal-M`,
+                  Sal_Bottom_ppth = `Sal-B`, Temp_Water_Top_degC = `TempWater-T`, Temp_Water_Mid_degC = `TempWater-M`, Temp_Water_Bottom_degC = `TempWater-B`, `Turbidity_JTU-NTU` = Turbidity, Turbidity_Q = TurbidityQ,
+                  Color_345_F45_PCU = `Color345_F45`, Color_345_F45_Q = `Color345_F45Q`
     )
 
   return(out)
