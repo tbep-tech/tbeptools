ce856021c4cc7e3b8c1218454eed944dfb203c05
fawda123 <mbafs2012@gmail.com>
2022-12-12 15:46:02 -0500

    update fim data for 2021

--- R/fimdata.R
+++ R/fimdata.R
@@ -1,8 +1,8 @@
-#' FIM data for Tampa Bay Nekton Index current as of 07262021
+#' FIM data for Tampa Bay Nekton Index current as of 12122022
 #'
-#' FIM data for Tampa Bay Nekton Index current as of 07262021
+#' FIM data for Tampa Bay Nekton Index current as of 12122022
 #'
-#' @format A data frame with 45945 rows and 19 variables:
+#' @format A data frame with 47844 rows and 19 variables:
 #' \describe{
 #'   \item{Reference}{chr}
 #'   \item{Sampling_Date}{Date}
