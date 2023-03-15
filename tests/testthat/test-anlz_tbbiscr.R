48f73a401b513bc664dba29efb7f4297993d6aa4
fawda123 <mbafs2012@gmail.com>
2023-03-14 20:29:12 -0400

    fix test for anlz_tbbiscr

--- tests/testthat/test-anlz_tbbiscr.R
+++ tests/testthat/test-anlz_tbbiscr.R
@@ -1,8 +1,9 @@
 test_that("Checking anlz_tbbiscr", {
 
   result <- tbbiscr[nrow(tbbiscr), ]
-  expect_equal(result, structure(list(StationID = 12744L, AreaAbbr = "MTB", FundingProject = "TBEP",
-                                      ProgramID = 18L, ProgramName = "Dredge Hole Study", Latitude = 27.822125,
+  expect_equal(result, structure(list(StationID = 12744L, StationNumber = "17DH06-6",
+                                      AreaAbbr = "MTB", FundingProject = "TBEP", ProgramID = 18L,
+                                      ProgramName = "Dredge Hole Study", Latitude = 27.822125,
                                       Longitude = -82.487685, date = structure(17247, class = "Date"),
                                       yr = 2017, TotalAbundance = 11075, SpeciesRichness = 31,
                                       TBBI = 88.01, TBBICat = "Healthy", Salinity = 28.87), row.names = c(NA,
