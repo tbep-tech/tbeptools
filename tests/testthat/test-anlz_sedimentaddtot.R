983e700502516d6bafd449b0328bb49cb6435192
fawda123 <mbafs2012@gmail.com>
2023-05-28 08:52:11 -0400

    fixing anlz_sedimentaddtot tests

--- tests/testthat/test-anlz_sedimentaddtot.R
+++ tests/testthat/test-anlz_sedimentaddtot.R
@@ -6,9 +6,9 @@ test_that("Checking anlz_sedimentaddtot", {
 
 test_that("Checking show_sedimentmap sanity checks", {
 
-  expect_error(show_sedimentaddtot(sedimentdata, param = 'Arsenic', yrrng = c(1990, 2030)))
-  expect_error(show_sedimentaddtot(sedimentdata, param = 'Arsenic', yrrng = c(2001, 1994)))
-  expect_error(show_sedimentaddtot(sedimentdata, param = 'asdf', yrrng = c(1993, 2000)))
+  expect_error(anlz_sedimentaddtot(sedimentdata, param = 'Arsenic', yrrng = c(1990, 2030)))
+  expect_error(anlz_sedimentaddtot(sedimentdata, param = 'Arsenic', yrrng = c(2001, 1994)))
+  expect_error(anlz_sedimentaddtot(sedimentdata, param = 'asdf', yrrng = c(1993, 2000)))
   expect_error(anlz_sedimentaddtot(sedimentdata, param = 'Arsenic', bay_segment = 'asdf'))
   expect_error(anlz_sedimentaddtot(sedimentdata, param = 'Arsenic', funding_proj = 'asdf'))
 
