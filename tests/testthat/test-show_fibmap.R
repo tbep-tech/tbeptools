bc3bad82b56497fcc92017d93912ce0a3a6e863e
fawda123 <mbafs2012@gmail.com>
2023-06-16 18:01:49 -0400

    adding test for show_fibmap

---
+++ tests/testthat/test-show_fibmap.R
@@ -0,0 +1,10 @@
+test_that("show_fibmap correctly creates a leaflet map", {
+
+  # Test if the function runs without errors
+  result <- show_fibmap(fibdata, yrsel = 2020, mosel = 7, areasel = 'Alafia')
+
+  # Check if the result is a leaflet object
+  expect_s3_class(result, "leaflet")
+
+})
+
