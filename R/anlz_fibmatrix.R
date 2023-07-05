8287184aab568b355265a8f3e8bd40a67a093b52
fawda123 <mbafs2012@gmail.com>
2023-07-05 15:27:28 -0400

    correct mwqa categories using likelihood in anlz_fibmatrix

--- R/anlz_fibmatrix.R
+++ R/anlz_fibmatrix.R
@@ -5,12 +5,13 @@
 #' @param fibdata input data frame as returned by \code{\link{read_importfib}}
 #' @param yrrng numeric vector indicating min, max years to include, defaults to range of years in \code{epcdata}
 #' @param stas numeric vector of stations to include, default as those relevant for the Hillsborough River Basin Management Action Plan, see details
+#' @param lagyr numeric for year lag to calculate categories, see details
 #'
 #' @concept show
 #'
 #' @return A \code{\link[dplyr]{tibble}} object with FIB summaries by year and station, including columns for the estimated geometric mean of fecal coliform concentrations (\code{gmean}), the proportions of samples exceeding 400 CFU / 100 mL (\code{exced}), the count of samples (\code{cnt}), and a category indicating a letter outcome based on the proportion of exceedences (\code{cat}).
 #'
-#' @details This function is used to create output for plotting a matrix stoplight graphic for FIB categories by station and year.  Each station and year is categorized based on the proportion of monthly samples where fecal indicator bacteria concentrations exceed 400 CFU / 100 mL (using Fecal Coliform, \code{fcolif} in \code{fibdata}).  The proportions are categorized as A, B, C, D, or E (Microbial Water Quality Assessment or MWQA categories) with corresponding colors, where the breakpoints for each category are <10\%, 10-30\%, 30-50\%, 50-75\%, and >75\% (right-closed).  See \code{\link{show_fibmatrix}} for additional details.
+#' @details This function is used to create output for plotting a matrix stoplight graphic for FIB categories by station and year.  Each station and year combination is categorized based on the likelihood of fecal indicator bacteria concentrations exceeding 400 CFU / 100 mL in a given year (using Fecal Coliform, \code{fcolif} in \code{fibdata}).  The proportions are categorized as A, B, C, D, or E (Microbial Water Quality Assessment or MWQA categories) with corresponding colors, where the breakpoints for each category are <10\%, 10-30\%, 30-50\%, 50-75\%, and >75\% (right-closed).  By default, the results for each year are based on a right-centered window that uses the previous two years and the current year to calculate probabilities using the monthly samples (\code{lagyr = 3}). See \code{\link{show_fibmatrix}} for additional details.
 #'
 #' @export
 #'
@@ -21,7 +22,8 @@
 #' @examples
 #' anlz_fibmatrix(fibdata)
 anlz_fibmatrix <- function(fibdata, yrrng = NULL,
-                           stas = c(143, 108, 107, 135, 118, 148, 105, 152, 137)){
+                           stas = c(143, 108, 107, 135, 118, 148, 105, 152, 137),
+                           lagyr = 3){
 
   geomean <- function(x){prod(x)^(1/length(x))}
 
@@ -34,61 +36,45 @@ anlz_fibmatrix <- function(fibdata, yrrng = NULL,
   if(any(!chk))
     stop('Station(s) not found in fibdata: ', paste(stas[!chk], collapse = ', '))
 
-  # Set time period at 1980-2011
+  # get geomean, proportion of sites > 400 cfu / 100mL, and prob of exceedence
+  # handles lagged calculations
   dat <- fibdata %>%
     dplyr::filter(epchc_station %in% stas) %>%
-    dplyr::filter(yr >= yrrng[1] & yr <= yrrng[2]) %>%
+    dplyr::filter(yr >= (yrrng[1] - (lagyr - 1)) & yr <= yrrng[2]) %>%
     dplyr::filter(!is.na(fcolif) | fcolif < 0) %>%
-    dplyr::mutate(
-      bin1 = dplyr::if_else(fcolif > 0 & fcolif <= 200, 1, 0),
-      bin2 = dplyr::if_else(fcolif > 200 & fcolif <= 400, 1, 0),
-      bin3 = dplyr::if_else(fcolif > 400 & fcolif <= 800, 1, 0),
-      bin4 = dplyr::if_else(fcolif > 800 & fcolif <= 2000, 1, 0),
-      bin5 = dplyr::if_else(fcolif > 2000, 1, 0)
-    ) %>%
-    select(yr, epchc_station, fcolif, bin1, bin2, bin3, bin4, bin5)
-
-  # Calculate summary information by epchc_station and year
-  station_binsums <- dat %>%
     summarise(
       gmean = geomean(fcolif),
-      sum_bin1 = sum(bin1),
-      sum_bin2 = sum(bin2),
-      sum_bin3 = sum(bin3),
-      sum_bin4 = sum(bin4),
-      sum_bin5 = sum(bin5),
+      sumgt400 = sum(fcolif > 400),
+      station_tot = dplyr::n(),
       .by = c('epchc_station', 'yr')
-    )
-
-  # Calculate ranked scores and check for exceedances
-  ranks <- station_binsums %>%
-    mutate(
-      station_tot = sum_bin1 + sum_bin2 + sum_bin3 + sum_bin4 + sum_bin5,
-      proprtn1 = sum_bin1 / station_tot,
-      proprtn2 = sum_bin2 / station_tot,
-      proprtn3 = sum_bin3 / station_tot,
-      proprtn4 = sum_bin4 / station_tot,
-      proprtn5 = sum_bin5 / station_tot,
-      sum345 = sum_bin3 + sum_bin4 + sum_bin5,
-      exceed_10_400_prob = pbinom(sum345 - 1, station_tot, 0.10, lower.tail = FALSE),
-      exceed_30_400_prob = pbinom(sum345 - 1, station_tot, 0.30, lower.tail = FALSE),
-      exceed_50_400_prob = pbinom(sum345 - 1, station_tot, 0.50, lower.tail = FALSE),
-      exceed_75_400_prob = pbinom(sum345 - 1, station_tot, 0.75, lower.tail = FALSE)
+    ) %>%
+    dplyr::arrange(epchc_station, yr) %>%
+    dplyr::mutate(
+      sumgt400 = stats::filter(sumgt400, rep(1, lagyr), sides = 1, method = 'convolution'),
+      station_tot = stats::filter(station_tot, rep(1, lagyr), sides = 1, method = 'convolution'),
+      .by = 'epchc_station'
+    ) %>%
+    dplyr::mutate(
+      exceed_10_400_prob = pbinom(sumgt400 - 1, station_tot, 0.10, lower.tail = FALSE),
+      exceed_30_400_prob = pbinom(sumgt400 - 1, station_tot, 0.30, lower.tail = FALSE),
+      exceed_50_400_prob = pbinom(sumgt400 - 1, station_tot, 0.50, lower.tail = FALSE),
+      exceed_75_400_prob = pbinom(sumgt400 - 1, station_tot, 0.75, lower.tail = FALSE)
     )
 
   # Put stations into binomial test groups based on significant exceedances of 400cfu criteria
-  ranks$MWQA <- NA
-  ranks$MWQA[ranks$exceed_10_400_prob >= 0.10] <- 'A'
-  ranks$MWQA[ranks$exceed_10_400_prob < 0.10 & ranks$exceed_30_400_prob >= 0.10] <- 'B'
-  ranks$MWQA[ranks$exceed_30_400_prob < 0.10 & ranks$exceed_50_400_prob >= 0.10] <- 'C'
-  ranks$MWQA[ranks$exceed_50_400_prob < 0.10 & ranks$exceed_75_400_prob >= 0.10] <- 'D'
-  ranks$MWQA[ranks$exceed_75_400_prob < 0.10] <- 'E'
+  dat$MWQA <- NA
+  dat$MWQA[dat$exceed_10_400_prob >= 0.10] <- 'A'
+  dat$MWQA[dat$exceed_10_400_prob < 0.10 & dat$exceed_30_400_prob >= 0.10] <- 'B'
+  dat$MWQA[dat$exceed_30_400_prob < 0.10 & dat$exceed_50_400_prob >= 0.10] <- 'C'
+  dat$MWQA[dat$exceed_50_400_prob < 0.10 & dat$exceed_75_400_prob >= 0.10] <- 'D'
+  dat$MWQA[dat$exceed_75_400_prob < 0.10] <- 'E'
 
-  out <- ranks %>%
+  out <- dat %>%
     dplyr::select(yr, epchc_station, gmean, cat = MWQA) %>%
     dplyr::mutate(
       epchc_station = factor(epchc_station, levels = stas)
     ) %>%
+    dplyr::filter(yr >= yrrng[1] & yr <= yrrng[2]) %>%
     tidyr::complete(yr = yrrng[1]:yrrng[2], epchc_station)
 
   return(out)
--- R/show_fibmatrix.R
+++ R/show_fibmatrix.R
@@ -15,7 +15,7 @@
 #'
 #' @return A static \code{\link[ggplot2]{ggplot}} object is returned by default. A \code{\link[reactable]{reactable}} table is returned if \code{asreact = TRUE}.  An interactive \code{\link[plotly]{plotly}} object is returned if \code{plotly = TRUE}.
 #'
-#' @details The matrix color codes years by station based on the proportion of monthly samples where fecal indicator bacteria concentrations exceed 400 CFU / 100 mL (using Fecal Coliform, \code{fcolif} in \code{fibdata}).  The proportions are categorized as A, B, C, D, or E (Microbial Water Quality Assessment or MWQA categories) with corresponding colors, where the breakpoints for each category are <10\%, 10-30\%, 30-50\%, 50-75\%, and >75\% (right-closed). Methods and rationale for this categorization scheme are provided by the Florida Department of Environmental Protection, Figure 8 in the document at \url{http://publicfiles.dep.state.fl.us/DEAR/BMAP/Tampa/MST\%20Report/Fecal\%20BMAP\%20DST\%20Final\%20Report\%20--\%20June\%202008.pdf}.
+#' @details The matrix color codes years and stations based on the likelihood of fecal indicator bacteria concentrations exceeding 400 CFU / 100 mL (using Fecal Coliform, \code{fcolif} in \code{fibdata}).  The likelihoods are categorized as A, B, C, D, or E (Microbial Water Quality Assessment or MWQA categories) with corresponding colors, where the breakpoints for each category are <10\%, 10-30\%, 30-50\%, 50-75\%, and >75\% (right-closed). By default, the results for each year are based on a right-centered window that uses the previous two years and the current year to calculate probabilities from the monthly samples (\code{lagyr = 3}).  Methods and rationale for this categorization scheme are provided by the Florida Department of Environmental Protection, Figure 8 in the document at \url{http://publicfiles.dep.state.fl.us/DEAR/BMAP/Tampa/MST\%20Report/Fecal\%20BMAP\%20DST\%20Final\%20Report\%20--\%20June\%202008.pdf} and Morrison et al. 2009 in the \href{https://drive.google.com/file/d/1vaoAKkwSLlIS2RzeBeCTjQST1dUmo0rr/view}{BASIS 5 proceedings}.
 #'
 #' The default stations are those used in TBEP report #05-13 (\url{https://drive.google.com/file/d/1MZnK3cMzV7LRg6dTbCKX8AOZU0GNurJJ/view}) for the Hillsborough River Basin Management Action Plan (BMAP) subbasins.  These include Blackwater Creek (WBID 1482, EPC stations 143, 108), Baker Creek (WBID 1522C, EPC station 107), Lake Thonotosassa (WBID 1522B, EPC stations 135, 118), Flint Creek (WBID 1522A, EPC station 148), and the Lower Hillsborough River (WBID 1443E, EPC stations 105, 152, 137).  Other stations in \code{fibdata} can be plotted using the \code{stas} argument.
 #'
@@ -27,13 +27,13 @@
 #' @examples
 #' show_fibmatrix(fibdata)
 show_fibmatrix <- function(fibdata, yrrng = NULL,
-                           stas = c(143, 108, 107, 135, 118, 148, 105, 152, 137), txtsz = 3,
-                           asreact = FALSE, nrows = 10, family = NA, plotly = FALSE, width = NULL,
-                           height = NULL){
+                           stas = c(143, 108, 107, 135, 118, 148, 105, 152, 137), lagyr = 3,
+                           txtsz = 3, asreact = FALSE, nrows = 10, family = NA, plotly = FALSE,
+                           width = NULL, height = NULL){
 
   cols <- c('#2DC938', '#E9C318', '#EE7600', '#CC3231', '#800080')
 
-  toplo <- anlz_fibmatrix(fibdata, yrrng = yrrng, stas = stas)
+  toplo <- anlz_fibmatrix(fibdata, yrrng = yrrng, stas = stas, lagyr = lagyr)
 
   # reactable object
   if(asreact){
