d5fece2b187aaf50dfe378d75d92c825f9b4201a
fawda123 <mbafs2012@gmail.com>
2023-08-23 09:56:42 -0400

    update fimstations with 2022 data

--- R/fimstations.R
+++ R/fimstations.R
@@ -2,7 +2,7 @@
 #'
 #' Spatial data object of FIM stations including Tampa Bay segments
 #'
-#' @format A simple features \code{\link[sf]{sf}} object (POINT) with 7124 features and 2 fields, +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
+#' @format A simple features \code{\link[sf]{sf}} object (POINT) with 7450 features and 2 fields, +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
 #' \describe{
 #'   \item{Reference}{num}
 #'   \item{bay_segment}{chr}
