c4f36be3a19b98eb9f634f0759a7b5eebe7d99af
fawda123 <mbafs2012@gmail.com>
2023-09-14 11:46:44 -0400

    added show_annualassess for annual water quality table

---
+++ R/show_annualassess.R
@@ -0,0 +1,67 @@
+#' Create a table for the annual management outcome assessments
+#'
+#' Create a table for the annual management outcome assessments for chlorophyll-a and light attenuation by bay segment
+#'
+#' @inheritParams anlz_yrattain
+#' @param caption logical indicating if a caption is added using \code{\link[flextable]{set_caption}}
+#' @param family optional chr string indicating font family for text labels
+#'
+#' @return A \code{\link[flextable]{flextable}} object showing the segment-averaged chlorophyll-a and light attenuation for the selected year, with bay segment names colored by the management outcome used in \code{\link{show_matrix}}.
+#'
+#' @importFrom dplyr %>%
+#'
+#' @concept show
+#'
+#' @export
+#'
+#' @examples
+#' show_annualassess(epcdata, yrsel = 2022)
+#' show_annualassess(epcdata, yrsel = 2022, caption = T)
+show_annualassess <- function(epcdata, yrsel, partialyr = F, caption = F, family = 'Arial'){
+
+  totab <- anlz_yrattain(epcdata, yrsel = yrsel, partialyr = partialyr) %>%
+    dplyr::mutate(
+      chla_val = round(chla_val, 1),
+      chla_target = round(chla_target, 1),
+      la_val = round(la_val, 2),
+      la_target = round(la_target, 2),
+      outcome = dplyr::case_when(
+        outcome == 'green' ~ '#2DC938',
+        outcome == 'yellow' ~ '#E9C318',
+        outcome == 'red' ~ '#CC3231'
+      )
+    )
+
+  outcome <- totab$outcome
+  totab <- totab %>%
+    dplyr::select(-outcome)
+
+  tab <- flextable::flextable(totab) %>%
+    flextable::bg(j = 'bay_segment', bg = outcome) %>%
+    flextable::add_header_row(colwidths = c(1, 2, 2), values = c('Segment', 'Chl-a (ug/L)', 'light')) %>%
+    flextable::compose(
+      i = 1, j = 4:5, part = "header",
+      value = flextable::as_paragraph("Light Penetration (m", flextable::as_sup("-1"), ')')
+    ) %>%
+    flextable::set_header_labels(values = c('', yrsel, 'target', yrsel, 'target')) %>%
+    flextable::align(align = 'center', part = 'all', j = 2:5)
+
+  if(caption){
+
+    cap.val <- paste0('Water quality outcomes for ', yrsel , '.')
+    tab <- tab %>%
+      flextable::set_caption(
+        flextable::as_paragraph(
+          flextable::as_chunk(cap.val, props = flextable::fp_text_default(font.family = family)
+          )
+        )
+      )
+
+  }
+
+  tab <- tab %>%
+    flextable::font(fontname = family, part = 'all')
+
+  return(tab)
+
+}
---
+++ tests/testthat/test-show_annualassess.R
@@ -0,0 +1,21 @@
+test_that("Function returns a valid flextable object", {
+  yrsel <- 2022
+  result <- show_annualassess(epcdata, yrsel)
+  expect_s3_class(result, "flextable")
+})
+
+test_that("Check segment colors", {
+  yrsel <- 1975
+  result <- show_annualassess(epcdata, yrsel)
+  result <- result$body$styles$cells$background.color$data[, 'bay_segment']
+  expected <- c("#CC3231", "#CC3231", "#CC3231", "#E9C318")
+  expect_equal(result, expected)
+})
+
+test_that("Check caption", {
+  yrsel <- 1975
+  result <- show_annualassess(epcdata, yrsel, caption = T)
+  result <- result$caption$value$txt
+  expected <- "Water quality outcomes for 1975."
+  expect_equal(result, expected)
+})
