fcac3423c1fce17e7edbd0ea9c68468a959a03b5
fawda123 <mbafs2012@gmail.com>
2024-02-15 08:29:36 -0500

    add city of st pete and fdep sw to available orgs in read_importwqp and util_orgin

--- R/util_orgin.R
+++ R/util_orgin.R
@@ -1,4 +1,4 @@
-#' Get organization name from organization identifier
+#' Get organization name from organization identifier in USEPA Water Quality Portal
 #'
 #' @param org chr string indicating the organization identifier, see \code{\link{read_importwqp}} for valid entries
 #' @param stanm logical indicating if a character string for a column name specific to the organization is returned
@@ -14,9 +14,9 @@
 #' util_orgin('21FLPASC_WQX', stanm = TRUE)
 util_orgin <- function(org, stanm = FALSE){
 
-  orgids <- c('21FLDOH_WQX', '21FLHILL_WQX', '21FLMANA_WQX', '21FLPASC_WQX', '21FLPDEM_WQX', '21FLPOLK_WQX')
-  orgname <- c('Florida DOH', 'Hillsborough', 'Manatee', 'Pasco', 'Pinellas', 'Polk')
-  stations <- c('fldoh', 'epchc', 'manco', 'pasco', 'pinco', 'polco')
+  orgids <- c('21FLCOSP_WQX', '21FLDOH_WQX', '21FLHILL_WQX', '21FLMANA_WQX', '21FLPASC_WQX', '21FLPDEM_WQX', '21FLPOLK_WQX', '21FLTPA_WQX')
+  orgname <- c('City of St. Petersburg', 'Florida DOH', 'Hillsborough', 'Manatee', 'Pasco', 'Pinellas', 'Polk', 'DEP Southwest')
+  stations <- c('stpet', 'fldoh', 'epchc', 'manco', 'pasco', 'pinco', 'polco', 'depsd')
   stations <- paste0(stations, '_station')
   names(stations) <- orgids
   names(orgname) <- orgids
