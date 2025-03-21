4a47630265362861bf62fbf3f93d4fd430b5b1b6
fawda123 <mbafs2012@gmail.com>
2025-03-21 18:01:08 -0400

    add function read_importrainmany

---
+++ R/read_importrainmany.R
@@ -0,0 +1,42 @@
+#' Run \code{read_importrain} for multiple years
+#'
+#' Run \code{read_importrain} for multiple years
+#'
+#' @param yrs numeric vector of years to download (do not need to be in order)
+#' @param quiet logical passed to \code{\link{read_importrain}} to suppress messages (default)
+#' @param sleep numeric number of seconds to pause between years
+#'
+#' @returns A data frame identical to that returned by \code{\link{read_importrain}} with the years requested
+#'
+#' @details This function is a simple wrapper for \code{\link{read_importrain}} to download multiple years of rainfall data from the Southwest Florida Water Management District's (SWFWMD) ftp site: ftp://ftp.swfwmd.state.fl.us/pub/radar_rainfall/Daily_Data/. The function will pause for \code{sleep} seconds between years to avoid overloading the server.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' read_importrainmany(c(2021, 2022), quiet = F)
+#' }
+read_importrainmany <- function(yrs, quiet = FALSE, sleep = 5){
+  yrs <- yrs
+  prcp_out <- list()
+
+  for(i in seq_along(yrs)){
+
+    yr = yrs[[i]]
+    prcptmp <- read_importrain(curyr = yr,
+                               catchpixels = catchpixels,
+                               quiet = quiet)
+
+    prcp_out[[i]] <- prcptmp
+    rm(prcptmp)
+
+    # pause for sleep seconds between years
+    Sys.sleep(sleep)
+
+  }
+
+  prcpdat <- dplyr::bind_rows(prcp_out)
+
+  return(prcpdat)
+
+}
---
+++ tests/testthat/test-read_importrainmany.R
@@ -0,0 +1,34 @@
+library(mockery)
+
+test_that("read_importrainmany works correctly", {
+  # Create a mock version of read_importrain
+  mock_read_importrain <- mock(
+    tibble::tibble(
+      station = c("station1", "station1", "station2"),
+      date = as.Date(c("2021-01-01", "2021-01-02", "2021-01-03")),
+      rain = c(0.1, 0.2, 0.3)
+    )
+  )
+
+  # Use with_mock instead of stub
+  with_mock(
+    "tbeptools::read_importrain" = mock_read_importrain,
+    {
+      # Call the function with mocked dependencies
+      result <- read_importrainmany(2021, quiet = FALSE)
+
+      # Define expected output
+      expected_output <- tibble::tibble(
+        station = c("station1", "station1", "station2"),
+        date = as.Date(c("2021-01-01", "2021-01-02", "2021-01-03")),
+        rain = c(0.1, 0.2, 0.3)
+      )
+
+      # Check if the result matches the expected output
+      expect_equal(result, expected_output)
+
+      # Verify that the mock read_importrain was called as expected
+      expect_called(mock_read_importrain, 1)
+    }
+  )
+})
