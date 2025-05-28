6e14ff982741ad6b3569c943b2ffaa8b377d2b33
fawda123 <mbafs2012@gmail.com>
2025-05-28 09:27:26 -0400

    add functions to retrieve data from FDEP WIN API, util_importwqwin, read_importwqwin

---
+++ R/read_importwqwin.R
@@ -0,0 +1,46 @@
+#' Retrieve water quality data from the Florida Department of Environmental Protection's Watershed Information Network (WIN)
+#'
+#' @param start_date character string for the start date in the format "YYYY-MM-DD"
+#' @param org_id character string for the organization ID
+#' @param verbose logical indicating whether to print verbose output
+#'
+#' @returns A data frame containing the water quality data
+#' @export
+#'
+#' @details This function implements \code{\link{util_importwqin}} iteratively to retrieve one to many pages of water quality results for the specified organization ID and start date. Data are retrieve using the API at <https://prodapps.dep.state.fl.us/dear-watershed/swagger-ui/index.html>.
+#' @examples
+#' \dontrun{
+#' dat <- read_importwqwin("2025-01-01", "21FLMANA", verbose = TRUE)
+#' head(dat)
+#' }
+read_importwqwin <- function(start_date, org_id, verbose = FALSE){
+
+  # Get results for the first page
+  if(verbose)
+    cat("Retrieving page 1...\n")
+
+  pg <- 0
+  res <- util_importwqwin(start_date, org_id, pg)
+
+  # start output
+  out <- data.frame()
+  out <- rbind(out, res$content)
+
+  # total pages
+  totpg <- res$totalPages
+
+  # Loop through remaining pages
+  while (res$last == FALSE) {
+
+    pg <- pg + 1
+    if(verbose)
+      cat(sprintf("Retrieving page %d of %d...\n", pg + 1, totpg))
+
+    res <- util_importwqwin(start_date, org_id, pg)
+    out <- rbind(out, res$content)
+
+  }
+
+  return(out)
+
+}
---
+++ R/util_importwqwin.R
@@ -0,0 +1,44 @@
+#' Utility function to retrieve water quality data from the Florida Department of Environmental Protection's Watershed Information Network (WIN)
+#'
+#' @param start_date character string for the start date in the format "YYYY-MM-DD"
+#' @param org_id character string for the organization ID
+#' @param page_num integer for the page number to retrieve
+#' @param verbose logical indicating whether to print verbose output
+#'
+#' @returns A list containing the results from the API request
+#'
+#' @export
+#'
+#' @details Used internally with \code{\link{read_importwqin}}.
+#'
+#' @concept util
+#'
+#' @examples
+#' \dontrun{
+#' onepg <- util_importwqwin("2025-01-01", "21FLMANA", 1)
+#' head(onepg)
+#' }
+util_importwqwin <- function(start_date, org_id, page_num) {
+
+  base_url <- "https://prodapps.dep.state.fl.us/dear-watershed/result-activities"
+
+  url <- sprintf("%s?ActivityStartDateFrom%%20%%28%%3E%%3D%%29=%s&Organization%%20ID=%s&page=%d&size=100&sort=resultKey%%2CASC",
+                 base_url,
+                 start_date,
+                 org_id,
+                 page_num
+  )
+
+  # Make the API request
+  response <- httr::GET(url)
+
+  if (httr::http_status(response)$category != "Success") {
+    stop(sprintf("API request failed with status: %s", httr::http_status(response)$message))
+  }
+
+  # Parse JSON response
+  out <- jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+
+  return(out)
+
+}
