1e6ff5c30b91856eb5eb97427985ea242903bfbc
fawda123 <mbafs2012@gmail.com>
2025-08-01 16:47:33 -0400

    fix test for anlz_refs to use internal file

--- tests/testthat/test-anlz_refs.R
+++ tests/testthat/test-anlz_refs.R
@@ -1,9 +1,9 @@
 test_that("Checking anlz_refs", {
 
-  path <- 'https://raw.githubusercontent.com/tbep-tech/tbep-refs/master/tbep-refs.csv'
+  path <- 'tbep-refs.csv'
   result <- anlz_refs(path) %>%
     .[(length(.) - 3):length(.)]
 
-  expect_equal(result, c("\tmisc={},", "\turl={https://tbeptech.org/TBEP_TECH_PUBS/1992/TBEP_01_92_FrameworkForCharacterization.pdf},",
+  expect_equal(result, c("\tmisc={},", "\turl={http://www.tbeptech.org/TBEP_TECH_PUBS/2019/TBEP_12-19_2018_Nekton_Index.pdf},",
                          "\ttype={Technical report},", "}\n"))
 })
