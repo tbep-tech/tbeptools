d5ca0a27eda75501a40b9426bdc1f14a8e0d50b0
fawda123 <mbafs2012@gmail.com>
2025-08-22 12:44:08 -0400

    add util_importwqwa for water atlas metadata from api #62

---
+++ R/util_importwqwa.R
@@ -0,0 +1,75 @@
+#' Retrieve metadata from the Water Atlas API
+#'
+#' @param endpoint Character indicating which API endpoint to retrieve, see details
+#' @param pageSize Numeric indicating the number of records to retrieve
+#' @param waterbodyId Optional numeric indicating a specific waterbody ID if requesting sampling locations
+#' 
+#' @details Endpoints include \code{"dataSources"}, \code{"parameters"}, \code{"sampling-locations"}, or \code{"waterbodies"}.
+#' 
+#' Data are retrieved in pages if the total number of records exceeds the page size.
+#' 
+#' @returns A data frame of results for the given endpoint
+#'
+#' @export
+#' 
+#' @examples
+#' # get available parameters
+#' util_importwqwa('parameters')
+#' 
+#' # get sampling locations for a specific waterbody
+#' waterbodies <- util_importwqwa('waterbodies')
+#' waterbodyid <- waterbodies |> 
+#'   dplyr::filter(grepl('Hillsborough Bay', name)) |>
+#'   dplyr::pull(id)
+#' util_importwqwa('sampling-locations', waterbodyId = waterbodyid)
+util_importwqwa <- function(endpoint, pageSize = 10000, waterbodyId = NULL){
+
+  endpoint <- match.arg(endpoint, c('dataSources', 'parameters', 'sampling-locations', 'waterbodies'))
+  
+  base_url <- "https://dev.api.wateratlas.org"
+
+  params <- list(
+    pageSize = pageSize,
+    pageNumber = 1
+  )
+  
+  if(!is.null(waterbodyId) & endpoint == 'sampling-locations'){
+    params$waterbodyId <- waterbodyId
+  }
+
+  url <- paste0(base_url, '/api/', endpoint)
+
+  response <- httr::GET(url, query = params)
+  httr::stop_for_status(response)
+  
+  data <- httr::content(response, as = "parsed")
+  
+  out <- dplyr::bind_rows(data$items)
+  
+  totpages <- data$totalPages
+  failed_pages <- c()
+  
+  if(totpages > 1){
+
+    for(i in 2:totpages){
+
+      params$pageNumber <- i
+  
+      response <- httr::GET(url, query = params)
+      
+      tryCatch({
+        httr::stop_for_status(response)
+        data <- httr::content(response, as = "parsed")
+        outi <- dplyr::bind_rows(data$items)
+        out <- dplyr::bind_rows(out, outi)
+        
+      }, error = function(e) {
+        failed_pages <<- c(failed_pages, i)
+        cat("Page", i, "of", totpages, "failed:", httr::status_code(response), "\n")
+      })
+    }
+  }
+
+  return(out)
+
+}
